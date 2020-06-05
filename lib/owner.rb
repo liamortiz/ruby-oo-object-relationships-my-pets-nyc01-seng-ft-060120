0f32979f534e6370c0c721828a9856d0a66b8b4c
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-05 02:33:55 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,35 @@
+require_relative './cat.rb'
+require_relative './dog.rb'
+
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  @@all = []
+  @@cats = []
+  @@dogs = []
+  
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @@all << self
+  end
+  
+  def say_species
+    "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def buy_cat
+    new_cat = Cat.new()
+  end
 end
